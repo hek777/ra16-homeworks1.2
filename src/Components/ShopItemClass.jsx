9ffcaeeb721bd7731331decfe874e4d461a6624c
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-01-24 21:50:27 +0300

    first commit

---
+++ src/Components/ShopItemClass.jsx
@@ -0,0 +1,50 @@
+import './main.css';
+
+import React from 'react';
+import PropTypes from'prop-types';
+import UserModel from '../models/UserModel';        
+
+
+export class ShopItemClass extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    // const item = {
+    //     brand: 'Tiger of Sweden',
+    //     title: 'Leonard coat',
+    //     description: 'Minimalistic coat in cotton-blend',
+    //     descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    //     price: 399,
+    //     currency: '£'
+      
+    render () {
+        const {item} = this.props;
+        return (
+        <>
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>  
+            <div className="description">
+                {item.descriptionFull}
+            </div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div class="purchase-info">
+              <div className="price">
+                 {item.currency}{item.price}
+             </div>
+             <button>Добавить в корзину</button>
+            </div>  
+        </div>     
+        </>
+    );
+}
+}
+
+ ShopItemClass.propTypes = {  
+     user: PropTypes.instanceOf(UserModel).isRequired
+ }
+ export default ShopItemClass;
---
+++ src/models/UserModel.js
@@ -0,0 +1,11 @@
+class UserModel {
+    constructor (id, brand, title, description, descriptionFull, price, currency  ) {
+        this.id = id;
+        this.brand = brand;
+        this.title = title;
+        this.description = description;
+        this.descriptionFull = descriptionFull;
+        this.price = price;
+        this.currency = currency;
+    }
+}
